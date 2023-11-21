0490d9d5b82a579db2819375748f25d6940044a6
M2Mdani <daniel.manchevski@m2mservices.com>
2023-11-21 23:34:29 +0200

    UDP both ways

---
+++ UDP-Client/UDP-Client/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpClient = new UdpClient();
+
+            // Change the server IP and port to match your server
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("212.116.138.116"), 4545);
+
+            while (true)
+            {
+                Console.Write("Enter a message to send (or 'exit' to quit): ");
+                string message = Console.ReadLine();
+
+                if (message.ToLower() == "exit")
+                    break;
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                udpClient.Send(data, data.Length, serverEndPoint);
+
+                // Receive the echoed message from the server
+                byte[] receivedData = udpClient.Receive(ref serverEndPoint);
+                string receivedMessage = Encoding.UTF8.GetString(receivedData);
+
+                Console.WriteLine($"Received from server: {receivedMessage}");
+            }
+
+            udpClient.Close();
+        }
+    }
+}
---
+++ UDP-Server/UDP-Server/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UdpClient udpServer = new UdpClient(8888);
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("UDP Server is listening...");
+
+            while (true)
+            {
+                byte[] data = udpServer.Receive(ref remoteEndPoint);
+                string message = Encoding.UTF8.GetString(data);
+
+                Console.WriteLine($"Received from {remoteEndPoint}: {message}");
+
+                // Echo back to the client
+                udpServer.Send(data, data.Length, remoteEndPoint);
+            }
+        }
+    }
+}
